5630a7477021411661b22995a1bebcf1d8696318
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-07 23:45:02 +0600

    add coffe

--- src/assets/Components/ADD Coffee/AddCoffe.jsx
+++ src/assets/Components/ADD Coffee/AddCoffe.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const AddCoffee = () => {
-  return <div>this is add coffee section</div>;
+  return (
+    <div className="bg-yellow-200 p-24">
+      <h2 className="text-2xl font-bold">Add a Coffee</h2>
+      <form action="">
+        <div className="md:flex">
+          <div className="form-control md:w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Coffee</span>
+            </label>
+            <label className="input-group w-full input-group-vertical">
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control md:w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input-group input-group-vertical">
+              <input
+                type="text"
+                name="quantity"
+                placeholder="Available Quantity"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
